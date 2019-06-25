82d53f27607394fec781fba5de63edd355b74a6a
13444841 <13444841@ermdc13.itd.uts.edu.au>
2019-06-25 17:52:05 +1000

    update pbs script

--- submit_medq.sh
+++ submit_medq.sh
@@ -2,13 +2,10 @@
 
 #PBS -l ncpus=4
 #PBS -l mem=64GB
-#PBS -l walltime=24:00:00
+#PBS -l walltime=96:00:00
 
 #PBS -m abe
 #PBS -M frederick.r.jaya@student.uts.edu.au
-
-OMP_NUM_THREADS=4
-export OMP_NUM_THREADS
 
 cd rec-bench
 nextflow run hcv_minimal_hpc.nf --with-timeline timeline.html --with-report report.html
