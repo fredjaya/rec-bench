24d6fffb0c90f8e80245876057649f65885dbd4f
Fred <fredjaya1@gmail.com>
2019-07-31 14:58:01 +1000

    prepping n1000 and n2500

---
+++ n1000.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#PBS -l ncpus=2
+#PBS -l mem=2GB
+#PBS -l walltime=12:00:00
+
+#PBS -m abe
+#PBS -M frederick.r.jaya@student.uts.edu.au
+
+cd ~/rec-bench
+nextflow rdm-n1000.nf -with-timeline out/n1000_timeline.html -with-report out/n1000_report.html -with-trace out/n1000_trace.html
