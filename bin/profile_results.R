4846084d563e88a6c04a7c46a362452991a9b8b2
Fred <fredjaya1@gmail.com>
2019-08-15 08:50:19 +1000

    profile viz scripts good, .sh typo
    profile script could be softcoded but ok for now

--- bin/profile_results.R
+++ bin/profile_results.R
@@ -3,6 +3,7 @@
 library(data.table)
 library(ggplot2)
 library(viridis)
+library(dplyr)
 
 #setwd("GitHub/rec-bench/")
 
@@ -13,33 +14,64 @@ df <- melt(df,
            variable.name = 'bp_position',
            value.name = 'pval'
            )
+
 df$bp_position <- gsub("X", "", df$bp_position)
-#df$bp_position <- sapply(df$bp_position, as.factor)
+df$bp_position <- as.numeric(df$bp_position)
 
-pdf("profile_test.pdf", onefile = F)
-df %>% 
-  group_by(mut, rec, seqLen) %>%
-  ggplot(.) +
-  facet_grid(cols = vars(rec), rows = vars(mut)) +
-  geom_point(aes(bp_position, pval))
-  
-  ggplot() +
-    geom_point(aes(bp_position, pval), alpha = 0.7, size = 3) +
-    scale_y_reverse() +
-    scale_colour_viridis(discrete = T) +
-    facet_grid(cols = vars(rec), rows = vars(mut))
-dev.off()
-
-  geom_point(alpha = 0.7, size = 3) +
+pdf("out/profile.pdf")
+ggplot(data = subset(df, seqLen == 100), aes(bp_position, pval)) +
+  geom_point(alpha = 0.9, size = 2, shape = 18) +
+  geom_line(aes(group = rep+seqLen)) +
   scale_y_reverse() +
-  scale_colour_viridis(discrete = T) +
-  facet_grid(cols = vars(rec), rows = vars(mut))
+  facet_grid(cols = vars(rec), rows = vars(mut)) +
+  geom_hline(yintercept = 0.05, colour = 'red') +
+  labs(title = "n = 100")
 
+ggplot(data = subset(df, seqLen == 1000), aes(bp_position, pval)) +
+  geom_point(alpha = 0.9, size = 2, shape = 18) +
+  geom_line(aes(group = rep+seqLen)) +
+  scale_y_reverse() +
+  facet_grid(cols = vars(rec), rows = vars(mut)) +
+  geom_hline(yintercept = 0.05, colour = 'red') +
+  labs(title = "n = 1000")
 
-ggplot(df, aes(bp_position, pval)) +
-  geom_boxplot(aes(colour = as.factor(seqLen)), alpha = 0.7, size = 2) +
+ggplot(data = subset(df, seqLen == 2500), aes(bp_position, pval)) +
+  geom_point(alpha = 0.9, size = 2, shape = 18) +
+  geom_line(aes(group = rep+seqLen)) +
+  scale_y_reverse() +
+  facet_grid(cols = vars(rec), rows = vars(mut)) +
+  geom_hline(yintercept = 0.05, colour = 'red') +
+  labs(title = "n = 2500")
+
+ggplot(data = subset(df, seqLen == 5000), aes(bp_position, pval)) +
+  geom_point(alpha = 0.9, size = 2, shape = 18) +
+  geom_line(aes(group = rep+seqLen)) +
+  scale_y_reverse() +
+  facet_grid(cols = vars(rec), rows = vars(mut)) +
+  geom_hline(yintercept = 0.05, colour = 'red') +
+  labs(title = "n = 5000")
+
+ggplot(data = subset(df, seqLen == 10000), aes(bp_position, pval)) +
+  geom_point(alpha = 0.9, size = 2, shape = 18) +
+  geom_line(aes(group = rep+seqLen)) +
+  scale_y_reverse() +
+  facet_grid(cols = vars(rec), rows = vars(mut)) +
+  geom_hline(yintercept = 0.05, colour = 'red') +
+  labs(title = "n = 10000")
+
+dev.off()
+#fail
+##### 
+seqnum <- unique(df$seqLen)
+pdf("profile_test.pdf", onefile = F)
+par(mfrow = c(5,1))
+for (i in seqnum) {
+p <- ggplot(df, aes(bp_position, pval)) +
+  geom_point(data = subset(df, seqLen == i), aes(colour = as.factor(seqLen)), alpha = 0.7, size = 2) +
   scale_y_reverse() +
   scale_colour_viridis(discrete = T) +
   facet_grid(cols = vars(rec), rows = vars(mut)) +
   geom_hline(yintercept = 0.05, colour = 'red')
-
+p
+}
+dev.off()
