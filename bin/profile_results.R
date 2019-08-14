bcc06aa9b346d65eb19881bbcd0658dd4b9dff8b
Fred <fredjaya1@gmail.com>
2019-08-14 16:51:19 +1000

    clean dir, add geneconv scripts, update profile viz script

--- bin/profile_results.R
+++ bin/profile_results.R
@@ -1,25 +1,45 @@
 #!/usr/bin/env Rscript
 
+library(data.table)
 library(ggplot2)
+library(viridis)
 
 #setwd("GitHub/rec-bench/")
-help = paste(
-  "phi_profile.R summarizes the results of a PhiPack 'Profile' analysis",
-  "Usage: Rscript phi_profile.R",
-  sep="\n")
-
-path = "out/E1_phipack/"
 
 # Read Profile.csv
-df <- read.csv(paste(path, 'Profile.csv', sep = ''), header = F)
-names(df) <- c('mid_bp', 'pvalue')
+df <- read.csv("out/profile_s_stats.csv", header = T, stringsAsFactors = T)
+df <- melt(df,
+           id = 1:5,
+           variable.name = 'bp_position',
+           value.name = 'pval'
+           )
+df$bp_position <- gsub("X", "", df$bp_position)
+#df$bp_position <- sapply(df$bp_position, as.factor)
 
-# Plot pvalues against putative breakpoint regions
-pdf(paste(path, 'Profile.pdf', sep = ''))
-ggplot(df, aes(mid_bp, pvalue)) +
-  geom_line() +
-  scale_y_reverse() +
-  geom_hline(yintercept = 0.05, color = 'red') +
-  theme_minimal()
+pdf("profile_test.pdf", onefile = F)
+df %>% 
+  group_by(mut, rec, seqLen) %>%
+  ggplot(.) +
+  facet_grid(cols = vars(rec), rows = vars(mut)) +
+  geom_point(aes(bp_position, pval))
+  
+  ggplot() +
+    geom_point(aes(bp_position, pval), alpha = 0.7, size = 3) +
+    scale_y_reverse() +
+    scale_colour_viridis(discrete = T) +
+    facet_grid(cols = vars(rec), rows = vars(mut))
 dev.off()
+
+  geom_point(alpha = 0.7, size = 3) +
+  scale_y_reverse() +
+  scale_colour_viridis(discrete = T) +
+  facet_grid(cols = vars(rec), rows = vars(mut))
+
+
+ggplot(df, aes(bp_position, pval)) +
+  geom_boxplot(aes(colour = as.factor(seqLen)), alpha = 0.7, size = 2) +
+  scale_y_reverse() +
+  scale_colour_viridis(discrete = T) +
+  facet_grid(cols = vars(rec), rows = vars(mut)) +
+  geom_hline(yintercept = 0.05, colour = 'red')
 
