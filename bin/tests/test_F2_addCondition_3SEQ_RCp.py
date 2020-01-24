050292e48da435733a63278ebeeaf8dd47150a0e
Fred <fredjaya1@gmail.com>
2020-01-24 11:27:28 +1100

    Update test_F2_addCondition_3SEQ_RCp.py
    fixed NameError issue - look into it for the def Read

--- bin/tests/test_F2_addCondition_3SEQ_RCp.py
+++ bin/tests/test_F2_addCondition_3SEQ_RCp.py
@@ -21,30 +21,35 @@ class testFunctions(unittest.TestCase):
         recFile.columns = newHeader
         return(recFile)
     
-    test_simSeq_unmatch = pd.Series({'params': 'msa_m0.0010_rc0.000010_n100_dual0.1_rep1.fasta.3s.rec',
-                                      'seq': 'seq_9999999',
-                              'breakpoints': float('NaN')})
-    test_simSeq_matchMultiBP = pd.Series({'params': 'msa_m0.010_rc0.010_n100_dual1_rep2.fasta.3s.rec',
-                                             'seq': 'seq_3',	
-                                     'breakpoints': 891})
+    def setUp(self):
+        self.test_simSeq_unmatch = pd.Series(
+                {'params': 'msa_m0.0010_rc0.000010_n100_dual0.1_rep1.fasta.3s.rec',
+                    'seq': 'seq_9999999',
+            'breakpoints': float('NaN')})
     
-    test_predictedRec_Norm    = readRecFile_testMod("/Users/13444841/Dropbox/Masters/02_working/2001_3seq_conditions/B2_3seq/RCp/msa_m0.0010_rc0.000010_n100_dual0.1_rep1.fasta.3s.rec")
-    test_predictedRec_MultiBP = readRecFile_testMod("/Users/13444841/Dropbox/Masters/02_working/2001_3seq_conditions/B2_3seq/RCp/msa_m0.010_rc0.010_n100_dual1_rep2.fasta.3s.rec")
-    test_predictedRec_DupSeq  = test_predictedRec_MultiBP.append(test_predictedRec_MultiBP)
+        self.test_simSeq_matchMultiBP = pd.Series(
+                {'params': 'msa_m0.010_rc0.010_n100_dual1_rep2.fasta.3s.rec',
+                    'seq': 'seq_3',	
+            'breakpoints': 891})
+    
+        self.test_predictedRec_Norm    = readRecFile_testMod("/Users/13444841/Dropbox/Masters/02_working/2001_3seq_conditions/B2_3seq/RCp/msa_m0.0010_rc0.000010_n100_dual0.1_rep1.fasta.3s.rec")
+        self.test_predictedRec_MultiBP = readRecFile_testMod("/Users/13444841/Dropbox/Masters/02_working/2001_3seq_conditions/B2_3seq/RCp/msa_m0.010_rc0.010_n100_dual1_rep2.fasta.3s.rec")
+        self.test_predictedRec_DupSeq  = self.test_predictedRec_MultiBP.append(
+                self.test_predictedRec_MultiBP)
     
     def test_getPredictedSeqs(self):
         ''' 
         Single row 
         '''
-        self.assertEqual(getPredictedSeqs(test_simSeq_unmatch,
-                                          test_predictedRec_Norm), ['seq_45'],
+        self.assertEqual(getPredictedSeqs(self.test_simSeq_unmatch,
+                                          self.test_predictedRec_Norm), ['seq_45'],
                          'Predicted sequences don\'t match')
     
         ''' 
         Multiple rows with simulated breakpoints appended 
         '''
-        self.assertEqual(getPredictedSeqs(test_simSeq_unmatch,
-                                          test_predictedRec_MultiBP), 
+        self.assertEqual(getPredictedSeqs(self.test_simSeq_unmatch,
+                                          self.test_predictedRec_MultiBP), 
                          ['seq_3', 'seq_8', 'seq_18', 'seq_41', 'seq_45', 
                           'seq_51', 'seq_70'],
                           'Predicted sequences don\'t match')
@@ -52,29 +57,30 @@ class testFunctions(unittest.TestCase):
         ''' 
         Duplicate sequence 
         '''
-        self.assertEqual(getPredictedSeqs(test_simSeq_unmatch, 
-                                          test_predictedRec_DupSeq), 
+        self.assertEqual(getPredictedSeqs(self.test_simSeq_unmatch, 
+                                          self.test_predictedRec_DupSeq), 
                                             -1)
         
     def test_getPredictedBPs(self):
         ''' 
-        Multiple breakpoints predicted, when one sequence matches only one 
+        Data with multiple breakpoints predicted, when one sequence matches only one 
         pair of bps 
         '''
-        self.assertEqual(getPredictedBPs(test_simSeq_matchMultiBP,
-                                         test_predictedRec_MultiBP),
+        self.assertEqual(getPredictedBPs(self.test_simSeq_matchMultiBP,
+                                         self.test_predictedRec_MultiBP),
                                         [44, 45, 46, 763, 764, 765])
         
         ''' 
-        
+        Data with multiple breakpoints predicted, and sequence matches more
+        than one breakpoint pairs
         '''
         
         
         ''' 
         When simSeq is not present in predictedRec
         '''
-        self.assertEqual(getPredictedBPs(test_simSeq_unmatch, 
-                                         test_predictedRec_MultiBP),
+        self.assertEqual(getPredictedBPs(self.test_simSeq_unmatch, 
+                                         self.test_predictedRec_MultiBP),
                                          -1)
 
 if __name__ == '__main__':
