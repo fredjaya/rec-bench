59980d7a5da441e27bea996de114f72448e6b2d4
13444841 <13444841@ermdc13.itd.uts.edu.au>
2020-03-03 12:03:15 +1100

    check_file good for S4_santa

--- bin/other_scripts/check_file_params.py
+++ bin/other_scripts/check_file_params.py
@@ -27,7 +27,6 @@ def get_file_names(path_to_dir, process_out):
 def params_regex(subject):
     p = re.sub(r'[a-z]+', '', subject)
     p = p.split("_")
-    print(p)
     if args.process_out == "S4_santa":
         return p[4:9]
 
@@ -45,12 +44,33 @@ def create_dict(file_names):
         full_dict[n_index] = sub_dict
         n_index += 1
     return full_dict     
- 
+
+def count_occurrences(full_dict):
+    c_mut = dict()
+    c_rec = dict()
+    c_seqn = dict()
+    c_dualInf = dict()
+    c_rep = dict()
+
+    for param in full_dict.values():
+        # https://stackoverflow.com/a/6582852
+        c_mut[param['mut']] = c_mut.get(param['mut'], 0) + 1
+        c_rec[param['rec']] = c_rec.get(param['rec'], 0) + 1
+        c_seqn[param['seqn']] = c_seqn.get(param['seqn'], 0) + 1
+        c_dualInf[param['mut']] = c_mut.get(param['mut'], 0) + 1
+        c_rep[param['rep']] = c_rep.get(param['rep'], 0) + 1
+        
+    print("----- MUTATION RATE -----\n{}\n \
+----- RECOMBINATION RATE -----\n{}\n \
+----- SEQUENCE NUMBER -----\n{}\n \
+----- DUAL INF RATE -----\n{}\n \
+----- REPLICATE -----\n{}\n".format(c_mut, c_rec, c_seqn, c_dualInf, c_rep))
+
 def main():
     intro(args.process_out, args.path_to_dir)
     file_names = get_file_names(args.path_to_dir, args.process_out)
     file_params = create_dict(file_names)
-    print(file_params)
+    count_occurrences(file_params)
     return
 
 # Main ----------
